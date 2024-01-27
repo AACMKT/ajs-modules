69d62639613f8892f5c882f71b236afdd85a7956
Maxim E <maermolenko@mail.ru>
2024-01-27 17:19:35 +0300

    Initial commit

---
+++ Webpack/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    mode: 'production',
+    entry: ['./src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                presets: [
+                    ['@babel/preset-env', { targets: "defaults" }]
+                ]
+                }
+        }
+        }
+        ]
+      },
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'style.css'
+    }),
+    ]
+  };
